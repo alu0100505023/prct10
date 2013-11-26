c896acf56c6fc411d0d3337581e942d27db19a59
Juan Lucas <alu0100499452@ull.edu.es>
2013-11-26 11:25:39 +0000

    Modificacion del coerce y test

--- lib/matriz_densa.rb
+++ lib/matriz_densa.rb
@@ -1,108 +1,140 @@
-require "matriz"
+require "matriz.rb"
 
-class matriz_densa < matriz;
+class Matriz_densa < Matriz
 
-	def +(other)
-		
-		r =@m
-	
-		for i in 0...@f do
-    			for j in 0...@c do
-        			r[i][j]=@m[i][j]+other.m[i][j]
-    			end
-		end
-		return r
-	end
+        def +(other)
+           
+			  
+			  case other
+			  when Matriz_densa
+					raise ArgumentError, "Las matrices no son iguales en longitud." unless @f == other.f && @c == other.c
+		           r =@m
+		     
+		          for i in 0...@f do
+		                   for j in 0...@c do
+		                         r[i][j]=@m[i][j]+other.m[i][j]
+		                   end
+		          end
+		          return r
+			  when Matriz_Dispersa
+					raise ArgumentError, "Las matrices no son iguales en longitud." unless @f == other.matrix.size && @c == other.matrix[1].vector.size
+					other + self
+				
+			  else
+					raise TypeError.new("No se puede pasar #{other.inspect} a Matriz")
+			  end
+        end
 
 
-	def -(other)
-		rs =@m
+        def -(other)
+          
+           case other
+			  when Matriz_densa
+					  raise ArgumentError, "Las matrices no son iguales en longitud." unless @f == other.f && @c == other.c
+		           rs =@m
+		     
+		          for i in 0...@f do
+		                   for j in 0...@c do
+		                         rs[i][j]=@m[i][j]+other.m[i][j]
+		                   end
+		          end
+		          return rs
+			  when Matriz_Dispersa
+					raise ArgumentError, "Las matrices no son iguales en longitud." unless @f == other.matrix.size && @c == other.matrix[1].vector.size
+					other - self
+				
+			  else
+					raise TypeError.new("No se puede pasar #{other.inspect} a Matriz")
+			  end
+        end
 
-		for i in 0...@f do
-    			for j in 0...@c do
-        			rs[i][j]=@m[i][j]-other.m[i][j]
-    			end
-		end
-		return rs
-	end
 
-
-	def *(other)
-		z = Array.new
-		for i in 0...@f do
-		   z[i] = Array.new
-		   for j in 0...@c do
-			z[i][j] = 0
-		   end
-		end
-		for i in 0...@f do
-        		for j in 0...@c do
-            			for k in 0...@f do
-                			z[i][j] += @m[i][k] * other.m[k][j]
-           			 end
-        		end
-    		end        
-		return z
-	end
-
-	
-	def to_s
-		for i in 0...@f do
-			for j in 0...@c do
-				print @m[i][j]	
-			end
-			puts
-		end
-		
-	end
-
-	def /(other)
-	
-		w = Array.new
-		for i in 0...@f do
-		   w[i] = Array.new
-		   for j in 0...@c do
-			w[i][j] = 0
-		   end
-		end
-
-		for i in 0...@f do
-        		for j in 0...@c do
-            			for k in 0...@f - 1 do
-                			w[i][j] += @m[i][k] * other.m[i][j]
-           			 end
-        		end
-    		end
-		return w       
-	end
-
-	def max(other)
-		
-		maximo = 0.to_f
-		for i in 0...@f do
-			for j in 0...@c do
-				if @m[i][j].to_f > maximo
-					maximo = @m[i][j].to_f
+        def *(other)				
+		  		case other
+				when Matriz_densa
+					raise ArgumentError, "Las columnas de una matriz no coinciden con las filas de la otra." unless @c == other.f
+                z = Array.new
+                for i in 0...@f do
+                   z[i] = Array.new
+                   for j in 0...@c do
+                        z[i][j] = 0
+                   end
+                end
+                for i in 0...@f do
+                        for j in 0...@c do
+                                    for k in 0...@f do
+                                        z[i][j] += @m[i][k] * other.m[k][j]
+                                    end
+                        end
+                    end        
+                return z
+				when Matriz_Dispersa
+					raise ArgumentError, "Las columnas de una matriz no coinciden con las filas de la otra." unless @c == other.matrix.size
+					other * self
+				else
+					raise TypeError.new("No se puede pasar #{other.inspect} a Matriz")
 				end
-			end
-		end
-		maximo
+        end
 
-	end
+        
+        def to_s
+                for i in 0...@f do
+                        for j in 0...@c do
+                                print @m[i][j]        
+                        end
+                        puts
+                end
+                
+        end
+
+        def /(other)
+        
+                w = Array.new
+                for i in 0...@f do
+                   w[i] = Array.new
+                   for j in 0...@c do
+                        w[i][j] = 0
+                   end
+                end
+
+                for i in 0...@f do
+                        for j in 0...@c do
+                                    for k in 0...@f - 1 do
+                                        w[i][j] += @m[i][k] * other.m[i][j]
+                                    end
+                        end
+                    end
+                return w       
+        end
+        
+
+        def max
+                
+                maximo = @m[0][0].to_f
+                for i in 0...@f do
+                        for j in 0...@c do
+                                if @m[i][j].to_f > maximo.to_f
+                                        maximo = @m[i][j]
+                                end
+                        end
+                end
+                maximo
+
+        end
 
 
-	def min(other)
+        def min
 
-		minimo = Maxint.to_f
-		for i in 0...@f do
-			for j in 0...@c do
-				if @m[i][j].to_f < minimo
-					minimo = @m[i][j].to_f
-				end
-			end
-		end
-		minimo
+                minimo = @m[0][0].to_f
+                for i in 0...@f do
+                        for j in 0...@c do
+                                if @m[i][j].to_f < minimo.to_f
+                                        minimo = @m[i][j]
+                                end
+                        end
+                end
+                minimo
 
-	end
+        end
 
 end
--- lib/matriz_dispersa.rb
+++ lib/matriz_dispersa.rb
@@ -1,81 +1,244 @@
-require "matriz.rb" 
+require 'Fraccion.rb'
+require 'matriz.rb'
+require "matrix"
 
-class matriz_dispersa << matriz;
+class SparseVector
+    attr_reader :vector
 
-	def coerce(other)
-		[self,other]
-	end
+    def initialize(h = {})
+        @vector = Hash.new(0)
+        @vector = @vector.merge!(h)
+    end
 
-	def +(other)
-		
-		r =@m
-	
-		for i in 0...@f do
-    			for j in 0...@c do
-        			r[i][j]=@m[i][j]+other.m[i][j]
-    			end
-		end
-		return r
-	end
+    def [](i)
+        @vector[i]
+    end
 
+    def to_s
+        @vector.to_s
+    end
+end
 
-	def -(other)
-		rs =@m
+class SparseMatrix < Matriz
 
-		for i in 0...@f do
-    			for j in 0...@c do
-        			rs[i][j]=@m[i][j]-other.m[i][j]
-    			end
-		end
-		return rs
-	end
+    attr_reader :matrix
 
+    def initialize(h = {})
+        @matrix = Hash.new({})
+        for k in h.keys do
+            @matrix[k] = if h[k].is_a? SparseVector 
+                                h[k]
+                        else
+                              @matrix[k] = SparseVector.new(h[k])
+                            end
+        end
+    end
 
-	def *(other)
-		z = Array.new
-		for i in 0...@f do
-		   z[i] = Array.new
-		   for j in 0...@c do
-			z[i][j] = 0
-		   end
-		end
-		for i in 0...@f do
-        		for j in 0...@c do
-            			for k in 0...@f do
-                			z[i][j] += @m[i][k] * other.m[k][j]
-           			 end
-        		end
-    		end        
-		return z
-	end
+    def [](i)
+        @matrix[i]
+    end
 
+    def col(j)
+        c = {}
+        for r in @matrix.keys do
+            c[r] = @matrix[r].vector[j] if @matrix[r].vector.keys.include? j
+        end
+        SparseVector.new c
+    end
 
-	def max(other)
-		
-		maximo = 0.to_f
-		for i in 0...@f do
-			for j in 0...@c do
-				if @m[i][j].to_f > maximo
-					maximo = @m[i][j].to_f
-				end
-			end
-		end
-		maximo
+        def comprobar
+                contar = 0
+                contartotal = 0
+                result = 0
+           for r in @matrix.keys do
+                         for j in @matrix[r].vector.keys do
+                                contartotal = contartotal + 1
+                                if @matrix[r].vector[j] == 0 
+                                        contar = contar + 1
+                                end
+                        end
+      end
+                result = (contar * 100) / contartotal
+                if result < 60 
+                         "La matriz no es dispersa"
+                end
+        end
 
-	end
+        def mostrar
+        for r in @matrix.keys do
+                                  for j in @matrix[r].vector.keys do
+                                        print "#{@matrix[r].vector[j]}  "  
+                                end
+                                puts
+        end
+        end
+        
+        def +(other)
+			  case other
+			  when Matriz_Dispersa
+					raise ArgumentError, "Las matrices no son iguales en longitud." unless @matrix.size == other.matrix.size && @matrix[1].vector.size == other.matrix[1].vector.size
+					resul = Array.new
+		     		for r in @matrix.keys do
+						resul_f = Array.new
+					  	for j in @matrix[r].vector.keys do        
+					  		#print "  #{@matrix[r].vector[j] + other.matrix[r].vector[j]}  "
+							resul_f << @matrix[r].vector[j] + other.matrix[r].vector[j]
+					  	end
+					  		#puts
+						resul << resul_f
+					end
+					return resul
+			  when Matriz_densa
+					raise ArgumentError, "Las matrices no son iguales en longitud." unless @matrix.size == other.f && @matrix[1].vector.size == other.c
+					resul = Array.new
+					for r in 1..other.f do
+						resul_f = Array.new
+               	for j in 1..other.c do
+               		#print "  #{@matrix[r].vector[j] + other.m[r - 1][j - 1]}"
+							resul_f << @matrix[r].vector[j] + other.m[r - 1][j - 1]
+               	end
+               	#puts
+						resul << resul_f
+        			end
+					return resul
+			  else
+					raise TypeError.new("No se puede pasar #{other.inspect} a Matriz")
+			  end
+        end
+        def -(other)
+			  case other
+			  when Matriz_Dispersa
+					raise ArgumentError, "Las matrices no son iguales en longitud." unless @matrix.size == other.matrix.size && @matrix[1].vector.size == other.matrix[1].vector.size
+					resul = Array.new
+		     		for r in @matrix.keys do
+						resul_f = Array.new
+					  	for j in @matrix[r].vector.keys do        
+					  		#print "  #{@matrix[r].vector[j] - other.matrix[r].vector[j]}  "
+							resul_f << @matrix[r].vector[j] - other.matrix[r].vector[j]
+					  	end
+					  		#puts
+						resul << resul_f
+					end
+					return resul
+			  when Matriz_densa
+					raise ArgumentError, "Las matrices no son iguales en longitud." unless @matrix.size == other.f && @matrix[1].vector.size == other.c
+					resul = Array.new
+					for r in 1..other.f do
+						resul_f = Array.new
+               	for j in 1..other.c do
+               		#print "  #{@matrix[r].vector[j] - other.m[r - 1][j - 1]}"
+							resul_f << @matrix[r].vector[j] - other.m[r - 1][j - 1]
+               	end
+               	#puts
+						resul << resul_f
+        			end
+					return resul
+			  else
+					raise TypeError.new("No se puede pasar #{other.inspect} a Matriz")
+			  end
 
+        end
 
-	def min(other)
+        def *(other) 
+        
+				case other
+				when Matriz_Dispersa
+					 raise ArgumentError, "Las columnas de una matriz no coinciden con las filas de la otra." unless @matrix[1].vector.size == other.matrix.size
+                sumatotal = 0
+                mul = Array.new                
 
-		minimo = @m[0][0]
-		for i in 0...@f do
-			for j in 0...@c do
-				if @m[i][j].to_f < minimo
-					minimo = @m[i][j].to_f
-				end
-			end
-		end
-		minimo
+                for i in @matrix.keys do
+                        mul_f = Array.new
+                     for j in @matrix[i].vector.keys do
+                                for k in @matrix[i].vector.keys do
+                                    suma = @matrix[i].vector[k] * other.matrix[k].vector[j]
+                                    sumatotal = sumatotal + suma;
+                                end
+                                mul_f << sumatotal
+                                sumatotal = 0
+                    end
+							mul << mul_f
+                end
+=begin        
+                for r in @matrix.keys do
+                   for j in @matrix[r].vector.keys do
+                      print "  #{mul[r][j]}  "
+                   end
+                   puts
+        			end
+=end
+					return mul
+			  when Matriz_densa
+					raise ArgumentError, "Las columnas de una matriz no coinciden con las filas de la otra." unless @matrix[1].vector.size == other.f
+					sumatotal = 0
+                mul = Array.new                
 
-	end
+                for i in @matrix.keys do
+                        mul_f = Array.new
+                     for j in @matrix[i].vector.keys do
+                           for k in @matrix[i].vector.keys do
+                              suma = @matrix[i].vector[k] * other.m[k - 1][j - 1]
+                              sumatotal = sumatotal + suma;
+                           end
+                           mul_f << sumatotal
+                           sumatotal = 0
+                    end
+							mul << mul_f
+                end 
+=begin       
+                for r in @matrix.keys do
+                   for j in @matrix[r].vector.keys do
+                      print "  #{mul[r][j]}  "
+                   end
+                   puts
+        			end
+=end
+					return mul
+			  else
+				  raise TypeError.new("No se puede pasar #{other.inspect} a Matriz")
+			  end
+        end
+
+        def max
+                   
+                maximo = @matrix[1].vector[1]
+           
+                for i in @matrix.keys do
+                        for j in @matrix[i].vector.keys do
+                           if @matrix[i].vector[j] > maximo
+                              maximo = @matrix[i].vector[j]
+                      end
+                   end
+                end
+					 return maximo
+        end
+
+		  def [](i,j)
+			
+				return @matrix[i].vector[j]
+			
+		  end
+
+        def min
+
+           minimo = @matrix[1].vector[1]
+                
+                for i in @matrix.keys do
+                        for j in @matrix[i].vector.keys do
+                           if @matrix[i].vector[j] < minimo
+                              minimo = @matrix[i].vector[j]
+                      end
+                   end
+                end
+					return minimo
+
+        end
+
+        def coerce(other)
+                [other,self]
+        end
+end
+
+class Matriz_Dispersa < SparseMatrix
+
 end
